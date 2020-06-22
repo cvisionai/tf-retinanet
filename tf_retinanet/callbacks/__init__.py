0a0b8ff61082f0f969ab25aff4b0c9b044801945
Benjamin Woodward <benjamin.woodward@cvisionconsulting.com>
2020-06-22 16:41:25 -0400

    add lr schedule, disable evaluation callback not implemented

--- tf_retinanet/callbacks/__init__.py
+++ tf_retinanet/callbacks/__init__.py
@@ -27,38 +27,50 @@ def get_callbacks(
 	validation_generator=None,
 	evaluation_callback=None,
 ):
-	""" Returns the callbacks indicated in the config.
-	Args
-		config              : Dictionary with indications about the callbacks.
-		model               : The used model.
-		prediction_model    : The used prediction model.
-		training_model      : The used training model.
-		validation_generator: Generator used during validation.
-		evaluation_callback : Callback used to perform evaluation.
-	Returns
-		The indicated callbacks.
-	"""
-	callbacks = []
+    """ Returns the callbacks indicated in the config.
+    Args
+            config              : Dictionary with indications about the callbacks.
+            model               : The used model.
+            prediction_model    : The used prediction model.
+            training_model      : The used training model.
+            validation_generator: Generator used during validation.
+            evaluation_callback : Callback used to perform evaluation.
+    Returns
+            The indicated callbacks.
+    """
+    callbacks = []
 
-	# Save snapshots of the model.
-	os.makedirs(os.path.join(config['snapshots_path'], config['project_name']))
-	checkpoint = tf.keras.callbacks.ModelCheckpoint(
-		os.path.join(
-			config['snapshots_path'],
-			config['project_name'],
-			'{epoch:02d}.h5'
-		),
-		verbose=1,
-	)
-	checkpoint = RedirectModel(checkpoint, model)
-	callbacks.append(checkpoint)
+    # Save snapshots of the model.
+    os.makedirs(os.path.join(config['snapshots_path'], config['project_name']))
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(
+            os.path.join(
+                    config['snapshots_path'],
+                    config['project_name'],
+                    '{epoch:02d}.h5'
+            ),
+            verbose=1,
+    )
+    checkpoint = RedirectModel(checkpoint, model)
+    callbacks.append(checkpoint)
+    lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(
+            monitor='loss',
+            factor=0.1,
+            patience=4,
+            verbose=1,
+            mode='min',
+            min_delta=0.01,
+            cooldown=0,
+            min_lr=1e-8)
+    callbacks.append(lr_scheduler)
 
-	# Evaluate the model.
-	if validation_generator:
-		if not evaluation_callback:
-			raise NotImplementedError('Standard evaluation_callback not implement yet.')
-		evaluation_callback = evaluation_callback(validation_generator)
-		evaluation_callback = RedirectModel(evaluation_callback, prediction_model)
-		callbacks.append(evaluation_callback)
+    '''
+    # Evaluate the model.
+    if validation_generator:
+        if not evaluation_callback:
+            raise NotImplementedError('Standard evaluation_callback not implement yet.')
+        evaluation_callback = evaluation_callback(validation_generator)
+        evaluation_callback = RedirectModel(evaluation_callback, prediction_model)
+        callbacks.append(evaluation_callback)
+    '''
 
-	return callbacks
+    return callbacks
